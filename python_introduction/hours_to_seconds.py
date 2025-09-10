8f3e74efe7b5828c9ee3b96607935dc4322fc354
Johnnjogu <Johnnjogu99@gmail.comm>
2025-09-10 15:30:02 +0300

    Changed the prompt to get input and then convert to int

--- python_introduction/hours_to_seconds.py
+++ python_introduction/hours_to_seconds.py
@@ -1,5 +1,8 @@
 #Prompt the user to input their current age
-current_age = int(input("How old are you?"))
+current_age = input("How old are you? ")
+
+int(current_age)  #Convert the input to an integer
+current_age = int(current_age)  #Reassign the converted value back to current_age
 
 #Calculate the age in 2050
 years_to_add = 2050 - 2023 # Assuming the current year is 2023
