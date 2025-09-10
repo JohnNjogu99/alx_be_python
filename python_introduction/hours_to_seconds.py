96a10eeb25cb53f768aae9558a9a5f43017ee132
Johnnjogu <Johnnjogu99@gmail.comm>
2025-09-10 15:57:43 +0300

    Convert Hours to Seconds

--- python_introduction/hours_to_seconds.py
+++ python_introduction/hours_to_seconds.py
@@ -1,11 +1,8 @@
-#Prompt the user to input their current age
-current_age = input("How old are you? ")
+#Number of  hours
+hours = 2
 
-int(current_age)  #Convert the input to an integer
-current_age = int(current_age)  #Reassign the converted value back to current_age
+#Convert hours to seconds
+seconds = hours * 3600
 
-#Calculate the age in 2050
-years_to_add = 2050 - 2023 # Assuming the current year is 2023
-furture_age = current_age + years_to_add
 #Print the result
-print(f"In 2050, you will be {furture_age} years old.")
+print(f"{hours} hour(s) is {seconds} seconds.")
