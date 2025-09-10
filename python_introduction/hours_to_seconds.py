cde9c311945866754b4b66d0c4698e6b43ee6670
Johnnjogu <Johnnjogu99@gmail.comm>
2025-09-10 14:19:20 +0300

    User Input Age Calculator

---
+++ python_introduction/hours_to_seconds.py
@@ -0,0 +1,8 @@
+#Prompt the user to input their current age
+current_age = int(input("How old are you?: "))
+
+#Calculate the age in 2050
+years_to_add = 2050 - 2023 # Assuming the current year is 2023
+furture_age = current_age + years_to_add
+#Print the result
+print(f"In 2050, you will be {furture_age} years old.")
