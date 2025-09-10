eab66c38a5dae9f54a1bf99ec1ecb13e407e8ebd
Johnnjogu <Johnnjogu99@gmail.comm>
2025-09-10 16:49:01 +0300

    added dollar sign

--- python_introduction/finance_calculator.py
+++ python_introduction/finance_calculator.py
@@ -1,8 +1,8 @@
 #Prompt the user for their monthly income
-monthly_income = float(input("Enter your monthly income: "))
+monthly_income = int(input("Enter your monthly income: "))
 
 #Prompt total monthly expenses
-total_expenses = float(input("Enter your total monthly expenses: "))
+total_expenses = int(input("Enter your total monthly expenses: "))
 
 # Calculate monthly savings
 monthly_savings = monthly_income - total_expenses
@@ -12,5 +12,5 @@ annual_savings = monthly_savings * 12
 projected_savings = annual_savings + (annual_savings * 0.05)
 
 # Step 4: Display the results
-print(f"Your monthly savings are: {monthly_savings}")
-print(f"Your projected savings after one year (with 5% interest) are: {projected_savings}")
+print(f"Your monthly savings are: ${int(monthly_savings)}")
+print(f"Your projected savings after one year (with 5% interest) are: ${int(projected_savings)}")
